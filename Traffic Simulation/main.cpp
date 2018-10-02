d397ac125a872978746bae9de04846ab992028f2
giorodriguez3898 <giorodriguez3898@gmail.com>
2018-10-01 17:19:17 -0700

    Add files via upload

--- Traffic Simulation/main.cpp
+++ Traffic Simulation/main.cpp
@@ -1,59 +1,87 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <cstdlib>
 #include "simulationHeader.h"
 #include <time.h>
 #include <windows.h>
 #include <random>
+#include <cmath>
 
 using namespace std;
 
-static float vel_mean = 17.8;
-static float vel_dev = 4.0;
-static float distance_mean = 4.0;
-static float distance_dev = 2.0;
-Car **mycars = new Car*[300];
-
-
+// setting my array of cars
+Car **mycars = new Car*[total_cars];
 int main() {                       
 
-	mycars[0] = new Car(vel_mean, distance_mean);
-	mycars[0]->printMe();
 
-	//Car::arrayAccesser(mycars);
+	// to keep track of how many cars are left to be instantiated
+	int auto_reduce = number_of_auton;               
+	int manual_reduce = number_of_manual;           
+	bool mySwitch = false;
 
+	// instantiates autonomous && manual cars by alternating 
+	for (int element = 0; element < total_cars; element++) {
 
-	/*for (int x = 0; x < 300; x++) {
-		mycars[x] = new Car( bellCurve(vel_mean, vel_dev), bellCurve(distance_mean, distance_dev) );        
-		// constructs cars and sets velocity and sets comfort distance 
-	}*/
-	
+		if (!(auto_reduce == 0) && (mySwitch == false)) {
+			mycars[element] = new Car(bellCurve(vel_mean, AutonVel_dev), 
+								bellCurve(distance_mean, autonDistance_dev), 
+								uniformNumber(lane_min, lane_max) );
+			auto_reduce--;
 
-/*	float y = 0.0
-	for (int x = 0; x < 300; x++) {
+			if (!(manual_reduce == 0)) {
+				mySwitch = true;
+			}
+		}
+		else if( !(manual_reduce == 0) && (mySwitch == true) ){
+			mycars[element] = new Car(bellCurve(vel_mean, Manualvel_dev), 
+								bellCurve(distance_mean, Manualdistance_dev),
+								uniformNumber(lane_min, lane_max) );
+			manual_reduce--;
 
-		Car::mycars[x]->setPos(y);
-		y =+ 15;
+			if (!(auto_reduce == 0)) {
+				mySwitch = false;
+			}
+		}
 	}
 
-	cout << "about to initiate a print me: " << endl;
-	cout << "" << endl;*/
-
-	//Car::mycars[5]->printMe();
-	//float vel1 = bellCurve(vel_mean, vel_dev);
-	//cout << "this is velocity 1: " << vel1 << endl;
-	//Car *car = new Car(vel1);
-
-	//float vel2 = bellCurve(vel_mean, vel_dev);
-	//cout << "this is velocity 2: " << vel2 << endl;
-	//Car *car2 = new Car(vel2);
+	// places cars randomly at a position between 25 meters and 125 meters
+	for (int element = 0; element < total_cars; element++) {
+		mycars[element]->setPos(uniformNumber(position_min, position_max));
+	}
 
 
-	//car->setDistance( bellCurve(distance_mean, distance_dev) );
-	//car2->setDistance( bellCurve(distance_mean, distance_dev) );
+	for (float time = 0; time < seconds_for_simulation; time = time + 1.0) {
 
+		cout << "" << endl;
+		cout << "Stats for time #" << time << endl;
+		
 
+		for (int element = 0; element < total_cars; element++) {
 
+			cout << "car number [" << element << "]" << endl;
+			mycars[element]->move(
+									time, 
+									car_ahead(mycars, mycars[element]), 
+									mycars[element],
+									mycars
+								);
+			mycars[element]->printMe();
+		}
+
+	}
+
+	float tracker = 0.0;
+	for (int x = 0; x < total_cars; x++) {
+		
+
+		if (mycars[x]->getPos() > 7800.0) {
+			tracker++;
+		}
+	}
+	float percentage = tracker / total_cars;
+
+	cout << "Percentage of cars that made it through: " << percentage << "%" << endl;
 
 	system("pause");
 
@@ -68,4 +96,14 @@ float bellCurve(float mean, float deviation) {
 
 	return number;
 
+}
+float uniformNumber(float minimum, float maximum) {
+
+	Sleep(80);
+	time_t t = clock();
+	std::default_random_engine generator(t);
+	std::uniform_real_distribution<double> distribution(minimum, maximum);
+
+	float b = distribution(generator);
+	return b;
 }
--- Traffic Simulation/simulationHeader.h
+++ Traffic Simulation/simulationHeader.h
@@ -2,11 +2,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
-#include <list>
-
 using namespace std;
 
+// use these to generate random velocity||distance in gaussian curve
+static float vel_mean = 17.8;
+static float Manualvel_dev = 4.0;
+static float AutonVel_dev = 1.0;
+
+static float distance_mean = 4.0;
+static float Manualdistance_dev = 2.5;
+static float autonDistance_dev = 1.0;
+
+// use these to generate a random number in uniform fashion
+static float lane_min = 1.1;
+static float lane_max = 2.9;
+static float position_min = 25.0;
+static float position_max = 125.0;
+
+// to adjust the settings && conditions for the simulation
+static float seconds_for_simulation = 30.0;
+static int total_cars = 100;
+static float percentage_of_auton = .10;
+static int number_of_auton = total_cars * percentage_of_auton;
+static int number_of_manual = total_cars - number_of_auton;
+
+
+// more forward declarations 
 float bellCurve(float mean, float deviation);
+float uniformNumber(float minimum, float maximum);
+class Car;
+bool car_difLane(Car** allcars, Car* currentCar);
 
 class Car {
 
@@ -14,39 +39,154 @@ private:
 	float vel;
 	float pos;
 	float comfort_distance;
+	int myLane;
 public:
-	//static Car* mycars[500];
-	//Car() {};                      // default constructor
-	Car(float velocity, float dis) // constructor to set vel and dis 
-	{ 
+
+	// car constructing 
+	Car(float velocity, float dis, int lane)
+	{
 		vel = velocity;
 		comfort_distance = dis;
+		myLane = lane;
 	}
-	static void arrayAccesser(Car* mycar) {
-		
-		mycar->printMe();
-	};
+
+	//setters and getters
 	void setPos(float position) { pos = position; }
 	float getPos() { return pos; }
 
-	//void setDistance(float dis) { comfort_distance = dis; }
+	void setLane(int Setlane) { myLane = Setlane; }
+	int getLane() { return myLane; }
+
 	float getDistance() { return comfort_distance; }
 
+	// print me for cars
 	void printMe() {
-		cout << "I am velocity: " << vel << endl;
-		cout << "position: " << pos << endl;	
-		cout << "I am comfort_distance: " << comfort_distance << endl;
-		cout << "" << endl;
+		//cout << "I am velocity: " << vel << endl;
+		cout << "position: " << pos << endl;
+		cout << "I am in lane: " << myLane << endl;
+		//cout << "I am comfort_distance: " << comfort_distance << endl;
+		//cout << "" << endl;
 	}
-	void move(float front, float time) { 
-		
-		if (front < (checker(time) + comfort_distance))
-			pos = front - comfort_distance;
-		else
-			pos = pos + (vel * time); 
 
+	// move cars along the road
+	void move(float time, Car* car_ahead, Car* currentcar, Car** allcars) {
+
+		if (car_ahead == NULL) {
+			pos = pos + (vel * time);
+		}
+		else
+		{
+			if ((currentcar->checker(time) + currentcar->getDistance() > car_ahead->getPos()) == true
+				&&
+				!(car_difLane(allcars, currentcar))) {
+
+				pos = car_ahead->getPos() - comfort_distance;
+			}
+			else
+			{
+				if (currentcar->checker(time) + currentcar->getDistance() > car_ahead->getPos() == false){
+				pos = pos + (vel * time);
+				}
+				else {
+					switch (currentcar->getLane()) {
+					case 1:
+						currentcar->setLane(2);
+						pos = pos + (vel * time);
+						break;
+
+					case 2:
+						currentcar->setLane(1);
+						pos = pos + (vel * time);
+						break;
+					default:
+						break;
+					}
+				}
+				
+			}
+		}
 	}
 
 	float checker(float time) { return (pos + (vel * time)); }
 
 };
+
+// essential forward declaration
+Car* car_ahead(Car** allCars, Car* currentCar);  
+
+// to find car ahead of chosen car
+Car* car_ahead(Car** allCars, Car* currentCar) {
+
+	
+	float closest_distance = 100000.0;
+	Car* temp = NULL;
+
+	for (int element = 0; element < total_cars; element++) {
+
+		if (allCars[element]->getLane() == currentCar->getLane()) {
+
+			if (currentCar->getPos() - allCars[element]->getPos() < 0.0      &&
+				abs(currentCar->getPos() - allCars[element]->getPos()) < closest_distance)
+			{
+				temp = allCars[element];
+				closest_distance = abs(currentCar->getPos() - allCars[element]->getPos());
+			}
+		}
+
+	}
+	return temp; 
+}
+bool car_difLane(Car** allcars, Car* currentCar) {
+
+	int L = currentCar->getLane();
+	bool space_available = false;
+	float closest_distance = 100000.0;
+	Car* temp = NULL;
+
+	switch (L) {
+	case 1: 
+		for (int element = 0; element < total_cars; element++) {
+			if (allcars[element]->getLane() == 2) {
+				if (currentCar->getPos() - allcars[element]->getPos() < 0.0      &&
+					abs(currentCar->getPos() - allcars[element]->getPos()) < closest_distance)
+				{
+					temp = allcars[element];
+					closest_distance = abs(currentCar->getPos() - allcars[element]->getPos());
+				}
+			}
+			}
+		break;
+	case 2:
+		for (int element = 0; element < total_cars; element++) {
+			if (allcars[element]->getLane() == 1) {
+				if (currentCar->getPos() - allcars[element]->getPos() < 0.0      &&
+					abs(currentCar->getPos() - allcars[element]->getPos()) < closest_distance)
+				{
+					temp = allcars[element];
+					closest_distance = abs(currentCar->getPos() - allcars[element]->getPos());
+				}
+			}
+		}
+
+		break;
+	default:
+		break;
+	}
+
+	if (temp == NULL) {
+		space_available = true;
+	}
+	else
+	{
+		if (currentCar->getPos() + currentCar->getDistance() > temp->getPos()) {
+			space_available = false;
+		}
+		else
+		{
+			space_available = true;
+		}
+	}
+
+	return space_available;
+}
+
